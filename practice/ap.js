15eecb1017552af9500f0cb4351ea82bda1b4c8a
bbmoon01 <akishevasafiya@gmail.com>
2024-06-04 23:24:33 +0500

    Delete practice directory

--- practice/ap.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            alert(`You clicked on ${this.textContent}`);
-        });
-    });
-});
